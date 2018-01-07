b659442e1078ebf7d9d79dd1332cde6555a63a2b
nk-for-insight <kirnosov@email.arizona.edu>
2018-01-07 15:07:47 -0800

    Improved test readability

--- practice_problems_test.py
+++ practice_problems_test.py
@@ -5,8 +5,10 @@ import unittest
 import practice_problems
 
 class CountAppearancesTest(unittest.TestCase):
-    def test(self):
-        self.assertEqual(practice_problems.count_appearances([1, 1, 3]), {1: 2, 3: 1})
+    def test_good_input_results_in_expected_counts(self):
+        expected_counts = {1: 2, 3: 1}
+        self.assertDictEqual(practice_problems.count_appearances([1, 1, 3]),
+                             expected_counts)
 
 if __name__ == '__main__':
     unittest.main()

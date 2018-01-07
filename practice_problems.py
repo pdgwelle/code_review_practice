91767d6fa31d5d5a2ab97abbbb0464783e476b60
nk-for-insight <kirnosov@email.arizona.edu>
2018-01-07 14:45:03 -0800

    Added a test and fixed bugs' solution

--- practice_problems.py
+++ practice_problems.py
@@ -1,9 +1,10 @@
 """Coding practice problems"""
 
 def count_appearances(elements):
-	element_counts_map = {}
-	for element in elements:
-		if element in element_counts_map:
-			element_counts_map[element] += 1
-	    else:
-	    	element_counts_map[element] = 0
+    element_counts_map = {}
+    for element in elements:
+        if element in element_counts_map:
+            element_counts_map[element] += 1
+        else:
+            element_counts_map[element] = 1
+    return element_counts_map

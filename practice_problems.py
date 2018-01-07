afe61e2da94bce07f2cf89208643b06c6b9accd3
“Nikita <kirnosov@email.arizona.edu>
2018-01-07 13:34:19 -0800

    Added practice_problems.py, which contains a solution to 'count number of occurences of the element in a list' proble.

---
+++ practice_problems.py
@@ -0,0 +1,9 @@
+"""Coding practice problems"""
+
+def count_appearances(elements):
+	element_counts_map = {}
+	for element in elements:
+		if element in element_counts_map:
+			element_counts_map[element] += 1
+	    else:
+	    	element_counts_map[element] = 0

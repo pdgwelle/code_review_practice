a2a206d84ea14d63d261d4c191abff3ae0124d53
Paul <pdgwelle@gmail.com>
2018-01-11 20:40:56 -0500

    Update practice_problems.py

--- practice_problems.py
+++ practice_problems.py
@@ -5,3 +5,7 @@ def count_appearances(elements):
     for element in elements:
         element_counts_map[element] = element_counts_map.get(element, 0) + 1
     return element_counts_map
+
+## Some miscellaneous comments that I think we should delete
+## Blah Blah Blah
+## Blah blah blah
